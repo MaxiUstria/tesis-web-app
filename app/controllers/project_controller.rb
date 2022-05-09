6f1f97121ae53fe94d0dadf861f56d119233a84e
gimecaroceli <gimecaroceli@gmail.com>
2022-05-09 18:37:54 -0300

    view all projects

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,4 +4,7 @@ class ProjectController < ApplicationController
 
   def show
   end
+
+  def all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'components/show_text'
   get 'project/new'
   get 'project/show'
+  get 'project/all'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
